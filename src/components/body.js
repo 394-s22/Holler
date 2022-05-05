27a553503b785e47dddc14ff7d26a3d31cb44161
Sm1te <liyijiannice@outlook.com>
2022-05-04 19:00:18 -0500

    header done

--- src/components/body.js
+++ src/components/body.js
@@ -2,7 +2,6 @@ import { Buttons } from "./buttons.js";
 import { PDFViewer } from "./pdf-viewer.js";
 import { PDFs } from "./pdfs-page.js";
 import {PDFUpload } from "./pdf-upload.js";
-
 /*
 This component will have all the important stuff. So then in App we can just do <Body/>
 */
@@ -11,6 +10,7 @@ export const Body = () => {
     return (
     <div>
         <PDFViewer/>
+        <Buttons/>
     </div>
     )
 }
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,17 @@
 /*
 Component to hold UI section / buttons on the right side of the screen when viewing a single PDF
 */
+import React from "react";
+import { Button } from "react-bootstrap";
 
 export const Buttons = () => {
+    console.log('test');
+    return (
+        <div className="buttons">
+            
+            <Button variant="primary" size="sm"></Button>
+        </div>
+    )    
     
 }
+
