ac80d22f87d4f046d52e5c0e6c1c08e3c4a73627
Railgun707 <73008040+Railgun707@users.noreply.github.com>
2022-05-08 23:45:40 -0500

    LMAO

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -10,7 +10,8 @@ export const Buttons = (props) => {
     if(props.visibility) {
         return (
             <div className="buttons">
-                
+                <p>PDF Location: The initial term of this Agreement commences as of the Effective Date and, unless terminated earlier pursuant to any express provision of this Agreement, shall continue until five (5) years following the Effective Date (the "Initial Term").</p>
+                <p>Expiration date: 2/10/19</p>
                 <Button variant="primary" size="lg" style={{ backgroundColor: "#00B1E1" }}>Request Signature</Button>
                 <Button variant="primary" size="lg" style={{ backgroundColor: "#00B1E1" }}>Delivery</Button>
             </div>
