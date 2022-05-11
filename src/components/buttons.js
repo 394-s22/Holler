a40046fd5ffc7584f6bf83e91ebd8f2ab5b26276
Sydney <sydney.rae.smith@gmail.com>
2022-05-11 18:35:06 -0500

    copied google code lol

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -17,6 +17,7 @@ export const Buttons = (props) => {
                 <p>Revenue Profit Sharing Description: {props.revDesc}</p>
                 <Button variant="primary" size="lg" style={{ backgroundColor: "#00B1E1" }}>Request Signature</Button>
                 <Button variant="primary" size="lg" style={{ backgroundColor: "#00B1E1" }}>Delivery</Button>
+                <Button variant="primary" size="lg" style={{ backgroundColor: "#00B1E1" }} onClick = {addCalendarEvent(props.date)} >Add Event to Calendar</Button>
             </div>
         ) 
     }
@@ -30,4 +31,44 @@ export const get_info = () => {
         </div>
     )
 }
+
+const addCalendarEvent = (date) => {
+    authenticate()
+        .then(loadClient())
+        .then(() => {
+            return gapi.client.calendar.events.insert({
+                "resource": {
+                    "end": {
+                        "date": date
+                    },
+                    "start": {
+                        "date": date
+                    }
+                }
+            })
+        })
+        .then(function(response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+          },
+          function(err) { console.error("Execute error", err); });
+        
+}
+
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("YOUR_API_KEY");
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+  });
 
