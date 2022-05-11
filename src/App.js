688ea76f9784ec8e15cd2097646148c415441a13
Lingyue <31970601+lingyue328@users.noreply.github.com>
2022-05-11 18:20:00 -0500

    payment with stripe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {Body} from './components/body.js'
 import Title from './components/title.js'
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 
 function App() {
+  const [product] = React.useState({
+    name: "Service Fee",
+    price: 0.99,
+    description: "Cool car"
+  });
+
+  async function handleToken(token, addresses) {
+    const response = await axios.post(
+      "https://ry7v05l6on.sse.codesandbox.io/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
 
   return (
     <div>
       <Title/>
       <Body/>
+      <div className="containerPayment">
+      <StripeCheckout
+        stripeKey="pk_test_4TbuO6qAW2XPuce1Q6ywrGP200NrDZ2233"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
+    </div>
     </div>
   );
 }
