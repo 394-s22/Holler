3578714eaaa63976e51fab4f0b9ac7ef973eb690
davidsods <dlsotir@gmail.com>
2022-05-24 17:11:26 -0500

    fill input

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -8,11 +8,24 @@ export const Highlight = (props) => {
     
     console.log("priinting props")
     console.log(props);
+
+    const handleClick = () => {
+        document.getElementsByClassName('rpv-core__minimal-button')[3].click()
+        const myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve('foo');
+            }, 300)});
+        return myPromise
+        
+    }
     return (
-        <Button onPress={handleClick} color = "green-500" variant="cta" size="lg" >Show Text</Button>
+        <Button onPress={() => handleClick().then(() => {
+            populate_search(props.info)
+        })} color = "green-500" variant="cta" size="lg" >Show Text</Button>
     );
 };
 
-const handleClick = () => {
-    document.getElementsByClassName('rpv-core__minimal-button')[3].click()
+const populate_search = (info) => {
+        console.log(document.getElementsByClassName('rpv-core__textbox')[0])
+        document.getElementsByClassName('rpv-core__textbox')[0].value = info
 }
